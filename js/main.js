d24f28a29bb7a289ea93e2efa9cd8c70fa5c4e99
Meghan <treasure1210@gmail.com>
2018-01-14 20:48:25 -0700

    Improve accessibility

--- js/main.js
+++ js/main.js
@@ -170,6 +170,7 @@ createRestaurantHTML = (restaurant) => {
 
   const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
+  name.setAttribute('tabindex', 0);
   li.append(name);
 
   const neighborhood = document.createElement('p');
